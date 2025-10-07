9fa21d51f774d3b118a9b98ffde5fc0ad259cdf7
Ankit Yadav <ankitofficial0271@gmail.com>
2025-10-07 18:15:22 +0530

    done with user services

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,32 @@
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
-import React from "react";
+import { getData } from "@/context/UserContext";
+import axios from "axios";
+
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const { user, setUser } = getData();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get("http://localhost:6969/user/", {
+          withCredentials: true,
+        });
+        console.log(res);
+        setUser(res.data.user);
+      } catch (error) {
+        console.log("caught error");
+         navigate("/login");
+      }
+    };
+
+    fetchUser();
+    
+  }, [navigate, setUser]);
   return (
     <div>
       <Navbar />
